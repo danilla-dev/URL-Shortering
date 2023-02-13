ade000e302e7e82b1789cbe70ec4312ab9848ec5
DanielKam <dkaminski38p@protonmail.com>
2023-02-13 23:10:12 +0100

    Create a prototype of add new user algorythm

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 localStorage.setItem('status', 'false')
+localStorage.setItem('users', `{ "username":"unknown" , "email":"unknown", "password":"unknown" }`)
 import './createElement.js'
 import './burgerMenu.js'
 import './getStartedBtns.js'
--- scripts/signUpForm.js
+++ scripts/signUpForm.js
@@ -10,6 +10,35 @@ const emailRegExp =
 // LINK TO PASS REGEXP https://www.thepolyglotdeveloper.com/2015/05/use-regex-to-test-password-strength-in-javascript/
 //
 
+let users =
+	'{ "users" : [{ "username":"John" , "email":"Doe", "password":"12345678" },{ "username":"John" , "email":"Doe", "password":"12345678" }]}'
+
+const addNewUser = () => {
+	// get all users from local storage
+	let usersMemory = localStorage.getItem('users')
+
+	//create new user
+	const newUser = `{ "username":"Wikisiki" , "email":"tomczakowa.love.pl", "password":"123jebacpsy" }`
+
+	// add new user to all users in local storage
+	usersMemory = usersMemory.concat(',', newUser)
+	localStorage.setItem('users', usersMemory)
+
+	//add all users from localstorage to text json format in local storage
+	let allUsersJson = `{ "users":[${usersMemory}]}`
+	localStorage.setItem('test0000', allUsersJson)
+
+	//get json object from local storage with all users
+	const usersJson = JSON.parse(localStorage.getItem('test0000'))
+	console.log(usersJson)
+}
+
+const countErrors = () => {
+	const errorsCount = document.querySelectorAll('.form-error').length
+	if (errorsCount === 0) {
+		addNewUser()
+	}
+}
 const checkPasswords = (pass1, pass2) => {
 	if (pass1.value !== pass2.value) {
 		showError(pass2, 'Passwords is not the same.')
@@ -51,5 +80,6 @@ submitBtn.addEventListener('click', e => {
 	checkLength(username, 5)
 	checkLength(password, 8)
 	checkPasswords(password, rePass)
+	countErrors()
 	// saveUser()
 })
