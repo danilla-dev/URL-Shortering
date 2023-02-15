666bd4571964ec05dec1abffe34c8199221dca84
DanillaKam <122106121+DanillaKam@users.noreply.github.com>
2023-02-15 08:29:39 +0100

    Test a functionality to replace user to main page after successfull register.

--- scripts/signUpForm.js
+++ scripts/signUpForm.js
@@ -32,7 +32,7 @@ const addNewUser = (username, email, password) => {
 	localStorage.setItem('allUsersJson', allUsersJson)
 	localStorage.setItem('status', 'true')
 
-	location.href = '/index.html'
+	window.location.replace('../index.html')
 	clearForm()
 }
 
